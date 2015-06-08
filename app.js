1ac2daec906e43e12ffad0873422a72a515192f2
Edward Brooks <edubrooks@edubrooks.com>
2015-06-08 11:48:03 -0700

    Primera página y favicon

--- app.js
+++ app.js
@@ -5,25 +5,23 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index'); //importar enrutadores
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //instalar generador de vistas EJS
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes); //instalar enrutadores
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,7 +34,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'development') {  //gestión de errores durante desarrollo
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -48,7 +46,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) { //gestión de errores durante produccion
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
