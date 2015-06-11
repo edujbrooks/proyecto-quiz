b79dd08b9071314c4a9a50d760e8f3c661a79223
Edward Brooks <edubrooks@edubrooks.com>
2015-06-11 06:35:16 -0700

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,31 +1,40 @@
 var models = require('../models/models.js');
 
-// GET /quizes
+//Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function (req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new error('No existe quizId = ' + quizId));
+			}
+		}
+	).catch(function(error){next(error);});
+}
+
+
+ // GET /quizes
 exports.index = function(req, res) {
   models.Quiz.findAll().then(function(quizes) {
     res.render('quizes/index.ejs', { quizes: quizes});
-  })
+  }
+  ).catch(function(error) {next(error);});
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', { quiz: quiz});
+		res.render('quizes/show', { quiz: req.quiz});
 	})
 };
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	/*if (req.query.respuesta === 'Roma') {
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}	*/
-	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		if (req.query.respuesta === quiz.respuesta) {
-			res.render('quizes/answer', {quiz:quiz, respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', {quiz:quiz, respuesta: 'Incorrecto'});
-		}		
-	})	
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,14 @@ router.get('/author', function(req, res) {
   res.render('author', {});
 });
 
+//autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
 /*
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 */
+//Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
