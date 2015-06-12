2d60b28845dd8f9bd4036024fb72898bb560c2e5
Edward Brooks <edubrooks@edubrooks.com>
2015-06-12 12:04:16 -0700

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -43,3 +43,19 @@ exports.answer = function(req, res) {
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
+
+exports.new = function(req, res) { 
+	var quiz = models.Quiz.build(
+	{pregunta: "pregunta", respuesta: "respuesta"}
+	);
+	res.render('quizes/new', {quiz:quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) { 
+	var quiz = models.Quiz.build(req.body.quiz);
+	//guarda en BBDD los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta","respuesta"]}).then( function () {
+		res.redirect('/quizes');
+	});
+};
