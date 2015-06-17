21a486443e823b9c3a7838f2f9a6645ed64b0fff
Edward Brooks <edubrooks@edubrooks.com>
2015-06-17 11:11:15 -0700

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,13 @@
-//GET /login  -formulario-
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next) {
+	if (req.session.user) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
 
+//GET /login  -formulario-
 exports.new = function (req, res) {
 	var errors = req.session.errors || {};
 	req.session.errors = {};
